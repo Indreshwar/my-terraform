4d8147951cf8dfde912d69df97ac3e91c7c6ac08
Indreshwar <seelamindreshwar@gmail.com>
2026-08-11 18:23:03 +0530

    creating an ec2 instance

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,21 +1,23 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.37.0"
-    }
+     aws = {
+      source = "hashicorp/aws"
+      version = "6.58.0"
+     }
   }
 }
 
+#provider block
 provider "aws" {
-  region = "ap-south-1"
-
+  region ="ap-south-1"
 }
-#creating an aws ec2 instance
-resource "aws_instance" "FirstVM" {
-  ami           = "ami-05d2d839d4f73aafb"
-  instance_type = "t3.micro"
+
+#create an instance
+resource "aws_instance" "server" {
+  ami= "ami-01a00762f46d584a1"
+  instance_type = "t3.small"
   tags = {
-    Name = "My_First_VM"
+    Name = "My-server"
   }
+  
 }
