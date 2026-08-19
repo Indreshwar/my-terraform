416034c7e1c2a930423089c37634fe950ab1c85f
Indreshwar <seelamindreshwar@gmail.com>
2026-08-19 12:03:13 +0530

    creating an eip and attaching to ec2

--- Example-5/main.tf
+++ Example-5/main.tf
@@ -1,14 +1,59 @@
-#creating an ec2 instance
-resource "aws_instance" "EIP-server" {
-  ami           = "ami-05d2d839d4f73aafb"
-  instance_type = "t3.micro"
+#creating a default vpc
+resource "aws_default_vpc" "my-vpc" {
   tags = {
-    Name = "EIP_server"
+    Name = "Default_my_vpc"
   }
 }
-#creating an Elastic Ip address
-resource "aws_eip" "My-EIP" {
-  #associating an EIP address to instance
-  instance = aws_instance.EIP-server.id
 
+#creating a security group
+resource "aws_security_group" "my-sg" {
+  name        = "my-security-group"
+  description = "allow ssh and http access"
+  vpc_id      = aws_default_vpc.my-vpc.id
+  #creating an inbound rule
+  ingress {
+    description = "Allow ssh access through port-22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "allow http access through port-80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#creating an instance
+resource "aws_instance" "my-ec2" {
+  ami                    = "ami-01a00762f46d584a1"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [aws_security_group.my-sg.id]
+  tags = {
+    Name = "my-server"
+  }
+}
+
+#creating an elastic ip
+resource "aws_eip" "my-eip" {
+  #domain indicates that this eip use in vpc
+  domain = "vpc"
+  tags = {
+    Name = "my-eip"
+  }
+}
+
+#creating an elasticip association
+resource "aws_eip_association" "my-eip-association" {
+  instance_id   = aws_instance.my-ec2.id
+  allocation_id = aws_eip.my-eip.id
 }
